11703cd6e7630c795256b74a56026b302f9f0a7e
liguoyu1 <liguoyu1@xiaomi.com>
2018-05-10 14:22:07 +0800

    filter App Category high frequency

--- src/main/scala/com/xiaomi/ad/keyword/marchinggiant/relevance/ExtractAppCategory.scala
+++ src/main/scala/com/xiaomi/ad/keyword/marchinggiant/relevance/ExtractAppCategory.scala
@@ -16,6 +16,7 @@ import com.twitter.scalding.Args
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.{SaveMode, SparkSession}
 import collection.JavaConverters._
+import org.apache.spark.sql.functions.udf
 
 object ExtractAppCategory {
   case class Category(catId: Int, catName: String, score: Double)
@@ -40,9 +41,27 @@ object ExtractAppCategory {
     val spark = SparkSession.builder().config(sparkConf).getOrCreate()
     import spark.implicits._
 
+    val data = spark.read.parquet(args("input_matrix"))
+    val appPackageTopN = data.filter($"sourceId"===3).select($"entityKey")
+      .map(r=>(r.getAs[String]("entityKey").split('|').last,1)).rdd.reduceByKey(_ + _).sortBy(-_._2).take(args("thread").toInt).map(r=>r._1).toSet
+    println("appPackage size:"+appPackageTopN.size)
+
+    val appIdTopN = data.filter($"sourceId"===5&&($"actionId"===1||$"actionId"===4)&&$"text".isNotNull).select($"text")
+      .map(r=>(r.getAs[String]("text"),1)).rdd.reduceByKey(_ + _).sortBy(-_._2).take(args("thread").toInt).map(r=>r._1).toSet
+    println("appid size:"+appIdTopN.size)
+    val appIdTopNBc = spark.sparkContext.broadcast(appIdTopN)
+    val appPackageTopNBc = spark.sparkContext.broadcast(appPackageTopN)
+    // udf
+    val datafilter = udf{(packageName:String, appId:String)=>
+      appIdTopNBc.value.contains(appId.toString)||appPackageTopNBc.value.contains(packageName)
+    }
     val appNew = spark.read.parquet(args("input"))
       .dropDuplicates(Seq("appId"))
-      .select($"appId", $"packageName", $"keywords", $"category", $"emiCategory", $"lda")
+      .filter(datafilter($"appId", $"packageName"))
+    println("--------------------------------------------------------------------------------------------")
+    println("result size:"+appNew.count())
+    println("--------------------------------------------------------------------------------------------")
+    appNew.select($"appId", $"packageName", $"keywords", $"category", $"emiCategory", $"lda")
       .as[AppExtensioNew]
       .repartition(1)
       .write.mode(SaveMode.Overwrite)
