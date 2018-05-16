569974592e2c84dd7da518eb5463f8f5d63d7141
liguoyu1 <liguoyu1@xiaomi.com>
2018-05-16 14:48:17 +0800

    jaccard distance caculator and add category threshold

--- src/main/scala/com/xiaomi/ad/keyword/marchinggiant/relevance/static/staticResultAppUser.scala
+++ src/main/scala/com/xiaomi/ad/keyword/marchinggiant/relevance/static/staticResultAppUser.scala
@@ -21,9 +21,9 @@ object staticResultAppUser {
     val spark = SparkSession.builder().config(spark_conf).getOrCreate()
     import spark.implicits._
 
-    val appPath = "/user/h_miui_ad/develop/liguoyu1/project/AppExtension/AppSimInput/appAdInfoCate_"+input_p("yestoday")
-    val userPath = "/user/h_miui_ad/develop/liguoyu1/project/AppExtension/UserInput/userCateInfo_"+input_p("yestoday")
-    val resultPath = "/user/h_miui_ad/develop/liguoyu1/project/AppExtension/Result/re_sim_user2appid_"+input_p("yestoday")
+    val appPath = input_p("appPath") //"/user/h_miui_ad/develop/liguoyu1/project/AppExtension/AppSimInput/appAdInfoCate_"+input_p("yestoday")
+    val userPath = input_p("userPath") //"/user/h_miui_ad/develop/liguoyu1/project/AppExtension/UserInput/userCateInfo_"+input_p("yestoday")
+    val resultPath = input_p("resultPath") //"/user/h_miui_ad/develop/liguoyu1/project/AppExtension/Result/re_sim_user2appid_"+input_p("yestoday")
     val appCount = spark.read.parquet(appPath).count()//getOrElse(0)
     val userCount = spark.read.parquet(userPath).count()//).getOrElse(0)
     val resultCount = spark.sparkContext.textFile(resultPath).count()//).getOrElse(0)
